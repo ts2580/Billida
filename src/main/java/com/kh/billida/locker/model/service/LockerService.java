0bfe696f5e89b403d270ebafae925a365936c40c
kijung <dlrlwjd1313@naver.com>
2021-11-15 16:00:56 +0900

    [이기정 오류 해결]

--- src/main/java/com/kh/billida/locker/model/service/LockerService.java
+++ src/main/java/com/kh/billida/locker/model/service/LockerService.java
@@ -1,30 +1,29 @@
-/*
- * package com.kh.billida.locker.model.service;
- * 
- * import java.util.List;
- * 
- * import com.kh.billida.locker.model.dto.Locker;
- * 
- * 
- * public interface LockerService {
- * 
- * //리스트 public List<Locker> imageList();
- * 
- * //보기
- * 
- * //등록
- * 
- * //수정
- * 
- * //사진 수정
- * 
- * //삭제
- * 
- * 
- * 
- * 
- * void uploadImage(Locker locker); void deleteImage(String fileName);
- * 
- * }
- * 
- */
+
+  package com.kh.billida.locker.model.service;
+  
+  import java.util.List;
+  
+  import com.kh.billida.locker.model.dto.Locker;
+  
+  
+  public interface LockerService {
+  
+  //리스트 public List<Locker> imageList();
+  
+  //보기
+  
+  //등록
+  
+  //수정
+  
+  //사진 수정
+  
+  //삭제
+  
+  
+  
+  void uploadImage(Locker locker); void deleteImage(String fileName);
+  
+  }
+  
+ 
