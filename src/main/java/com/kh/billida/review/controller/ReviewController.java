d98b705ec7ea3198bff15b3d4124f4c9a563be23
soeun <thdms12333@gmail.com>
2021-11-15 23:37:15 +0900

    [소은] 리뷰 패키지 생성

---
+++ src/main/java/com/kh/billida/review/controller/ReviewController.java
@@ -0,0 +1,32 @@
+package com.kh.billida.review.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.billida.main.model.service.MainService;
+import com.kh.billida.review.model.service.ReviewService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("review")
+public class ReviewController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final ReviewService reviewService;
+	
+	@GetMapping("/review-form")
+	public void reviewForm() {};
+	
+	@PostMapping("/review-form")
+	public String uploadReview() {
+		return "/";
+	}
+	
+
+}
---
+++ src/main/java/com/kh/billida/review/model/service/ReviewServiceImpl.java
@@ -0,0 +1,8 @@
+package com.kh.billida.review.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewServiceImpl implements ReviewService{
+
+}
