6dd75511b3eda23c73c04a340f469d2c161b4081
soeun <thdms12333@gmail.com>
2021-11-21 20:36:14 +0900

    [소은]메인 컬럼명 수정

--- src/main/java/com/kh/billida/main/model/dto/Main.java
+++ src/main/java/com/kh/billida/main/model/dto/Main.java
@@ -16,7 +16,7 @@ public class Main {
 	private String lockerSize; //사이즈
 	private int lockerPassword; //비밀번호
 	private String location; //세부안내
-	private LocalDate rentableDateStart; //임대가능일자
+	private LocalDate rentableDateStart; //임대시작일자
 	private LocalDate rentableDateEnd; //임대종료
 	private String latitude; //위도
 	private String longitude; //경도
