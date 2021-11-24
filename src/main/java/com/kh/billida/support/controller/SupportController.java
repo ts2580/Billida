0b94fc9efb0fc1b7fac1b0df1ae4cbb28268d44e
BaeSeokJin <htdocs16bsj@naver.com>
2021-11-24 11:36:43 +0900

    [배석진] SupportController 오류 해결

--- src/main/java/com/kh/billida/support/controller/SupportController.java
+++ src/main/java/com/kh/billida/support/controller/SupportController.java
@@ -2,20 +2,12 @@ package com.kh.billida.support.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kh.billida.support.model.dto.Support;
-import com.kh.billida.support.model.service.SupportService;
-
-import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("support")
 public class SupportController {
-
-	private final SupportService supportService;
 	
 	// 문의&신고 페이지 접속
 	@GetMapping("support-index")
@@ -26,11 +18,11 @@ public class SupportController {
 	public void reportMain() {}
 	
 	// 신고 내용 입력 후 전송
-	@PostMapping("report-main")
-	public String reportInsert(Support support) {
-		supportService.reportInsert(support);
-		return "report-main";
-	}
+	//@PostMapping("report-main")
+	//public String reportInsert(Support support) {
+	//	supportService.reportInsert(support);
+	//	return "report-main";
+	//}
 	
 	// 신고 게시판 목록
 	@GetMapping("report-board")
