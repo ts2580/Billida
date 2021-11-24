fb92178f0242bbd96e2a91bb2a48ef358977b5f4
BaeSeokJin <htdocs16bsj@naver.com>
2021-11-24 11:32:02 +0900

    [배석진] 신고하기 기능 넣는중..(수정중)

--- src/main/java/com/kh/billida/support/model/service/SupportServiceImpl.java
+++ src/main/java/com/kh/billida/support/model/service/SupportServiceImpl.java
@@ -2,7 +2,19 @@ package com.kh.billida.support.model.service;
 
 import org.springframework.stereotype.Service;
 
+import com.kh.billida.support.model.dto.Support;
+import com.kh.billida.support.model.repository.SupportRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class SupportServiceImpl {
 
+	private final SupportRepository supportRepository;
+	
+	public void reportInsert(Support support) {
+		supportRepository.reportInsert(support);
+	}
+	
 }
