ebfd7ed7779205c9b224f69c6418d0ce4fe4743b
soeun <thdms12333@gmail.com>
2021-11-27 00:13:08 +0900

    [소은]리뷰 글자수체크&css 색상수정

--- src/main/webapp/resources/js/review/review-modifyForm.js
+++ src/main/webapp/resources/js/review/review-modifyForm.js
@@ -26,6 +26,7 @@ function drawStar(target){
 		 score.value = '5';
 	 }
 	 
+	 
 	let content = $('.review_textarea').val();
 	$('#counter').html("(" + content.length + " /최대 100자)"); //글자수 실시간 체크
  
